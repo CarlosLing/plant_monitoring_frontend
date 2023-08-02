26bdce34ef6b30e3961fb185742d4ac5b589d17d
Carlos García Ling <carlosgling@Carloss-MacBook-Air.local>
2023-08-02 13:25:33 +0200

    Add Navigation component, Document color pallete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import { Routes, Route } from 'react-router-dom';
 
 import Home from "./routes/home/home.component";
+import Navigation from './components/functional/NavigationBar/NavigationBar.component';
+
+const Detail = () => {
+  return (
+    <h1>TODO: Develop Detail page for sensor</h1>
+  );
+};
 
 const App = () => {
   return (
     <Routes>
-      <Route path='/' index element={<Home />} />
+      <Route path="/" element={<Navigation />} >
+        <Route index element={<Home />} />
+        <Route path="detail" element={<Detail />} />
+      </Route>
+
     </Routes>
   );
 };
--- src/components/functional/NavigationBar/NavigationBar.component.jsx
+++ src/components/functional/NavigationBar/NavigationBar.component.jsx
@@ -1,1 +1,27 @@
 import { Fragment } from "react";
+
+import { Outlet, Link } from 'react-router-dom';
+
+import logo from '../../../assets/logo_with_name.png';
+import "./NavigationBar.styles.scss"
+
+const Navigation = () => {
+    return (
+        <Fragment>
+            <div className='navigation'>
+                <Link className='logo-container' to='/'>
+                    <img className='logo-image' src={logo} alt='Logo error' />
+                </Link>
+                <div className='nav-links-container'>
+                    <Link className='nav-link' to='/detail'>
+                        Sensor Detail
+                    </Link>
+                </div>
+            </div>
+            <Outlet />
+        </Fragment>
+    );
+};
+
+
+export default Navigation; 
